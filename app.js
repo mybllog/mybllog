3a23a79125da1ed36b40e8aaddd10354214e47d8
Chisom <chisomeleanora@gmail.com>
2023-05-13 09:21:18 +0100

    building a backend for my landing page

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+
+
+
+
+app.listen(3005, () => {
+  console.log("this app is listening on port 3005");
+});
